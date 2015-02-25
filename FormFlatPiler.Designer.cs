c2a3ef9c9e93f53d5eb9ef5a4e1bf6624de5aebb
Thomas Wojnar <wojnarthomas@gmail.com>
2015-02-25 04:04:18 -0500

    Lex for keywords. Print function. Some design stuff.

--- FormFlatPiler.Designer.cs
+++ FormFlatPiler.Designer.cs
@@ -40,15 +40,17 @@
             this.taInput.Location = new System.Drawing.Point(12, 35);
             this.taInput.Multiline = true;
             this.taInput.Name = "taInput";
-            this.taInput.Size = new System.Drawing.Size(205, 164);
+            this.taInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.taInput.Size = new System.Drawing.Size(205, 186);
             this.taInput.TabIndex = 0;
             this.taInput.TextChanged += new System.EventHandler(this.taInput_TextChanged);
             // 
             // buttonCompile
             // 
-            this.buttonCompile.Location = new System.Drawing.Point(12, 205);
+            this.buttonCompile.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCompile.Location = new System.Drawing.Point(12, 227);
             this.buttonCompile.Name = "buttonCompile";
-            this.buttonCompile.Size = new System.Drawing.Size(75, 23);
+            this.buttonCompile.Size = new System.Drawing.Size(205, 49);
             this.buttonCompile.TabIndex = 1;
             this.buttonCompile.Text = "FlatPile!";
             this.buttonCompile.UseVisualStyleBackColor = true;
@@ -59,6 +61,7 @@
             this.taOutput.Location = new System.Drawing.Point(384, 35);
             this.taOutput.Multiline = true;
             this.taOutput.Name = "taOutput";
+            this.taOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.taOutput.Size = new System.Drawing.Size(264, 334);
             this.taOutput.TabIndex = 2;
             this.taOutput.TextChanged += new System.EventHandler(this.taOutput_TextChanged);
@@ -66,9 +69,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(96, 24);
             this.label1.TabIndex = 3;
             this.label1.Text = "Input Area";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -76,13 +80,14 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(381, 19);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(380, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.Size = new System.Drawing.Size(111, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "Output Area";
             // 
-            // Form1
+            // FormFlatPiler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -92,7 +97,7 @@
             this.Controls.Add(this.taOutput);
             this.Controls.Add(this.buttonCompile);
             this.Controls.Add(this.taInput);
-            this.Name = "Form1";
+            this.Name = "FormFlatPiler";
             this.Text = "FlatPiler";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- Lex.cs
+++ Lex.cs
@@ -10,9 +10,13 @@ namespace FlatPiler
 {
     class Lex
     {
+        private static Regex isDigit = new Regex("^[0-9]$");
+        private static Regex isChar = new Regex("^[a-z]$");
+
         public string inputString;
         public TextBox taOutput;
         public ArrayList tokens = new ArrayList();
+        public int errorCount = 0;
 
         public Lex(string inputString, TextBox taOutput)
         {
@@ -25,32 +29,48 @@ namespace FlatPiler
             this.taOutput.Text += (Environment.NewLine + this.inputString);
             if (this.inputString.Length == 0)
             {
-                this.taOutput.Text += (Environment.NewLine + "~~~Error, this program is empty.");
+                print("~~~Error, this program is empty.");
             }
             else
             {
                 char currentChar;
                 // string currentString = "";
-                Regex isDigit = new Regex("^[0-9]$");
-                Regex isChar = new Regex("^[a-z]$");
-
-                for (int i = 0; i < this.inputString.Length; i++) 
+                for (int i = 0; i < this.inputString.Length; i++)
                 {
-                    currentChar = this.inputString[i];
-                    // currentString += currentChar;
-                    this.taOutput.Text += (Environment.NewLine + "symbol: \"" + currentChar.ToString() + "\"");
-
-                    if (isDigit.IsMatch(currentChar.ToString())) 
+                    if (this.errorCount == 0)
                     {
-                        this.taOutput.Text += (Environment.NewLine + "-num: " + currentChar.ToString());
-                        this.tokens.Add(new Token(currentChar.ToString(), "digit"));
+                        currentChar = this.inputString[i];
+                        // currentString += currentChar;
+                        this.taOutput.Text += (Environment.NewLine + "symbol: \"" + currentChar.ToString() + "\"");
+                        if (currentChar.ToString().Equals(" "))
+                        {
+                        }
+                        else if (isDigit.IsMatch(currentChar.ToString()))
+                        {
+                            print("-num: " + currentChar.ToString());
+                            this.tokens.Add(new Token(currentChar.ToString(), "digit"));
+                        }
+                        else if (isChar.IsMatch(currentChar.ToString()) && !isChar.IsMatch(nextChar(i)))
+                        {
+                            print("-id: " + currentChar.ToString());
+                            this.tokens.Add(new Token(currentChar.ToString(), "id"));
+                        }
+                        else if (isChar.IsMatch(currentChar.ToString()) && isChar.IsMatch(nextChar(i)))
+                        {
+                            matchKeyWord(i);
+                            if (this.tokens.Count > 0)
+                            {
+                                string lastTokenValue = ((Token)this.tokens[(this.tokens.Count - 1)]).value;
+                                i += lastTokenValue.Length - 1;
+                                print("-word: " + lastTokenValue);
+                            }
+                        }
                     }
-                    else if (isChar.IsMatch(currentChar.ToString()) && !isChar.IsMatch(nextChar(i)))
+                    else
                     {
-                        this.taOutput.Text += (Environment.NewLine + "-id: " + currentChar.ToString());
-                        this.tokens.Add(new Token(currentChar.ToString(), "id"));
+                        print("There was an error, execution stopped.");
+                        i = this.inputString.Length;
                     }
-
                 }
             }
         }
@@ -58,10 +78,71 @@ namespace FlatPiler
         private string nextChar(int index)
         {
             char returnChar = ' ';
-            if (index + 1 != this.inputString.Length) {
+            if (index + 1 != this.inputString.Length)
+            {
                 returnChar = this.inputString[index + 1];
             }
             return returnChar.ToString();
+        }
+
+        private int nextWordIndex(int index)
+        {
+            int offset = index;
+            while (offset < this.inputString.Length && isChar.IsMatch(this.inputString[offset].ToString()))
+            {
+                offset++;
+            }
+            return offset - index;
+        }
+
+        private void matchKeyWord(int index)
+        {
+            int offset = nextWordIndex(index);
+            string suspectKeyword = this.inputString.Substring(index, offset);
+            // this.taOutput.Text += (Environment.NewLine + "symbol: \"" + suspectKeyword + "\"");
+            // this.tokens.Add(new Token(suspectKeyword, "meow"));
+            if (suspectKeyword == "int")
+            {
+                this.tokens.Add(new Token(suspectKeyword, "int"));
+            }
+            else if (suspectKeyword == "string")
+            {
+                this.tokens.Add(new Token(suspectKeyword, "string"));
+            }
+            else if (suspectKeyword == "boolean")
+            {
+                this.tokens.Add(new Token(suspectKeyword, "boolean"));
+            }
+            else if (suspectKeyword == "print")
+            {
+                this.tokens.Add(new Token(suspectKeyword, "print"));
+            }
+            else if (suspectKeyword == "if")
+            {
+                this.tokens.Add(new Token(suspectKeyword, "if"));
+            }
+            else if (suspectKeyword == "while")
+            {
+                this.tokens.Add(new Token(suspectKeyword, "while"));
+            }
+            else if (suspectKeyword == "false")
+            {
+                this.tokens.Add(new Token(suspectKeyword, "false"));
+            }
+            else if (suspectKeyword == "true")
+            {
+                this.tokens.Add(new Token(suspectKeyword, "true"));
+            }
+            else
+            {
+                print("~~~Error: " + suspectKeyword + " is not valid");
+                this.errorCount++;
+            }
+        }
+
+        private void print(string message)
+        {
+            this.taOutput.Text += (Environment.NewLine + message);
         }
     }
     class Token
