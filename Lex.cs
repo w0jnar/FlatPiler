d36767d78a57350bdb742c4bec412492cc601835
Thomas Wojnar <wojnarthomas@gmail.com>
2015-02-25 00:58:19 -0500

    More Lex, getting into Regexs.

--- Lex.cs
+++ Lex.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,15 +11,55 @@ namespace FlatPiler
     class Lex
     {
         public string inputString;
-        public TextBox outputField;
+        public TextBox taOutput;
+        public ArrayList tokens = new ArrayList();
 
-        public Lex(string input, TextBox output) {
-            inputString = input;
-            outputField = output;
+        public Lex(string inputString, TextBox taOutput)
+        {
+            this.inputString = Regex.Replace(inputString, @"\s+", "");
+            this.taOutput = taOutput;
         }
 
-        public void analysis() {
-            outputField.Text += (Environment.NewLine + this.inputString);
+        public void analysis()
+        {
+            // taOutput.Text += (Environment.NewLine + this.inputString);
+            if (this.inputString.Length == 0)
+            {
+                taOutput.Text += (Environment.NewLine + "~~~Error, this program is empty.");
+            }
+            else
+            {
+                char currentChar;
+                string currentString = "";
+                Regex isDigit = new Regex("^[0-9]*$");
+
+                for (int i = 0; i < this.inputString.Length; i++) 
+                {
+                    currentChar = this.inputString[i];
+                    // currentString += currentChar;
+                    if (isDigit.IsMatch(currentChar.ToString())) 
+                    {
+                        taOutput.Text += (Environment.NewLine + "num");
+                    }
+
+                }
+            }
+        }
+    }
+    class Token
+    {
+        public string type;
+        public string value;
+
+        public Token(string token, string type, int lineNumber, int position)
+        {
+            this.type = type;
+            this.value = token;
+        }
+
+        public override string ToString()
+        {
+            return "Type: " + this.type + " Value: " + this.value;
         }
     }
 }
