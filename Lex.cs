75c8ac59b40c978d3b37beba3d30e952fd79a75e
Thomas Wojnar <wojnarthomas@gmail.com>
2015-02-25 02:13:57 -0500

    More Lex, this time ids.

--- Lex.cs
+++ Lex.cs
@@ -16,34 +16,52 @@ namespace FlatPiler
 
         public Lex(string inputString, TextBox taOutput)
         {
-            this.inputString = Regex.Replace(inputString, @"\s+", "");
+            this.inputString = Regex.Replace(inputString, @"\s+", " ");
             this.taOutput = taOutput;
         }
 
         public void analysis()
         {
-            // taOutput.Text += (Environment.NewLine + this.inputString);
+            this.taOutput.Text += (Environment.NewLine + this.inputString);
             if (this.inputString.Length == 0)
             {
-                taOutput.Text += (Environment.NewLine + "~~~Error, this program is empty.");
+                this.taOutput.Text += (Environment.NewLine + "~~~Error, this program is empty.");
             }
             else
             {
                 char currentChar;
-                string currentString = "";
-                Regex isDigit = new Regex("^[0-9]*$");
+                // string currentString = "";
+                Regex isDigit = new Regex("^[0-9]$");
+                Regex isChar = new Regex("^[a-z]$");
 
                 for (int i = 0; i < this.inputString.Length; i++) 
                 {
                     currentChar = this.inputString[i];
                     // currentString += currentChar;
+                    this.taOutput.Text += (Environment.NewLine + "symbol: \"" + currentChar.ToString() + "\"");
+
                     if (isDigit.IsMatch(currentChar.ToString())) 
                     {
-                        taOutput.Text += (Environment.NewLine + "num");
+                        this.taOutput.Text += (Environment.NewLine + "-num: " + currentChar.ToString());
+                        this.tokens.Add(new Token(currentChar.ToString(), "digit"));
+                    }
+                    else if (isChar.IsMatch(currentChar.ToString()) && !isChar.IsMatch(nextChar(i)))
+                    {
+                        this.taOutput.Text += (Environment.NewLine + "-id: " + currentChar.ToString());
+                        this.tokens.Add(new Token(currentChar.ToString(), "id"));
                     }
 
                 }
             }
+        }
+
+        private string nextChar(int index)
+        {
+            char returnChar = ' ';
+            if (index + 1 != this.inputString.Length) {
+                returnChar = this.inputString[index + 1];
+            }
+            return returnChar.ToString();
         }
     }
     class Token
@@ -51,7 +69,7 @@ namespace FlatPiler
         public string type;
         public string value;
 
-        public Token(string token, string type, int lineNumber, int position)
+        public Token(string token, string type)
         {
             this.type = type;
             this.value = token;
