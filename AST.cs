14c1ee673d424c4657c9959a98b835a05f57e684
Thomas Wojnar <wojnarthomas@gmail.com>
2015-02-26 17:03:44 -0500

    AST print for Int Expr.

--- AST.cs
+++ AST.cs
@@ -26,10 +26,10 @@ namespace FlatPiler
             print("~~~Starting To Build AST.");
 
             this.root = new Node("Statement List");
-            // buildBlockTree(this.root);
+            buildBlockTree(this.root);
 
             print("~~~Ending AST Building." + Environment.NewLine + Environment.NewLine);
-            // this.root.PrintPretty("", true, taOutput);
+            this.root.PrintPretty("", true, taOutput);
         }
 
         private void buildBlockTree(Node root)
@@ -50,21 +50,74 @@ namespace FlatPiler
             {
                 buildStatementTree(root);
                 currentToken = (Token)this.tokens[this.tokenIndex];
+                // Console.Write(currentToken.value);
             }
             while (!currentToken.match("right_brace"))
             {
                 buildStatementTree(root);
                 currentToken = (Token)this.tokens[this.tokenIndex];
+                // Console.Write(currentToken.value);
             }
         }
 
-        private void buildStatementTree(Node root) 
+        private void buildStatementTree(Node root)
         {
             Token currentToken = (Token)this.tokens[this.tokenIndex];
-            if (currentToken.match("print")) 
+            // Console.Write(((Token)this.tokens[this.tokenIndex]).value);
+            if (currentToken.match("print"))
             {
-
+                print("--Building Print Node.");
+                Node printNode = new Node("Print");
+                buildPrintStatementTree(printNode);
+                // Console.Write(((Token)this.tokens[this.tokenIndex]).value);
             }
+        }
+
+        private void buildPrintStatementTree(Node root)
+        {
+            // Move past print.
+            this.tokenIndex++;
+            // Skip left paren.
+            this.tokenIndex++;
+            // Console.Write(((Token)this.tokens[this.tokenIndex]).value);
+            buildExprTree(root);
+            // Skip right paren.
+            this.tokenIndex++;
+        }
+
+        private void buildExprTree(Node root)
+        {
+            Token currentToken = (Token)this.tokens[this.tokenIndex];
+            if (currentToken.match("digit"))
+            {
+                buildIntExprTree(root);
+            }
+        }
+
+        private void buildIntExprTree(Node root)
+        {
+            Token currentToken = (Token)this.tokens[this.tokenIndex++];
+            Node digitNode = new Node(currentToken.value);
+            Token nextToken = (Token)this.tokens[this.tokenIndex];
+            if (nextToken.match("plus_op"))
+            {
+                Node plusOpNode = new Node("+");
+                plusOpNode.addChild(digitNode);
+
+                this.tokenIndex++;
+                buildIntExprTree(plusOpNode);
+            }
+            else
+            {
+                root.addChild(digitNode);
+            }
+        }
+
+        private void buildEndNode(Node root)
+        {
+            Token currentToken = (Token)this.tokens[this.tokenIndex++];
+            Node digitValueNode = new Node(currentToken.value);
+            root.addChild(digitValueNode);
         }
 
         private void print(string message)
