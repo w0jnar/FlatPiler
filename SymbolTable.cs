863abe169ee4a91bc55f04159f4490bccb388c25
Thomas Wojnar <wojnarthomas@gmail.com>
2015-03-06 18:53:32 -0500

    VarDecl and Assignment done.
    Started (finished?) print(), just have to do expr for that, and all of
    the things involving expr. Also WhileIf.

--- SymbolTable.cs
+++ SymbolTable.cs
@@ -54,22 +54,157 @@ namespace FlatPiler
                 {
                     generateBlock(root.children[i]);
                 }
+                else if (root.children[i].name == "Variable Declaration")
+                {
+                    generateVarDecl(root.children[i]);
+                }
+                else if (root.children[i].name == "Assignment")
+                {
+                    generateAssignment(root.children[i]);
+                }
+                else if (root.children[i].name == "Print")
+                {
+                    generatePrint(root.children[i]);
+                }
+
+                if (this.errorCount > 0)
+                {
+                    break;
+                }
             }
 
             if (this.errorCount == 0)
             {
                 print("~~~Closing scope " + this.currentScope + ".");
-                print("~~~Scope contained:");
-                for (int i = 0; i < this.scopes[this.currentScope].scopeMembers.Count; i++)
+                if (this.scopes[this.currentScope].scopeMembers.Count > 0)
                 {
-                    print(this.scopes[this.currentScope].scopeMembers[i]);
+                    print("~~~Scope contained:");
+                    for (int i = 0; i < this.scopes[this.currentScope].scopeMembers.Count; i++)
+                    {
+                        print(this.scopes[this.currentScope].scopeMembers[i]);
+                    }
+                }
+                else
+                {
+                    print("~~~Scope did not contain any ids.");
                 }
                 this.currentScope = this.scopes[this.currentScope].parentId;
             }
             else
             {
-                print("~~~Error: There was a Symbol Table generation error.");
+                // print("~~~Error: There was a Symbol Table generation error.");
             }
+        }
+
+        private void generateVarDecl(Node root)
+        {
+            String type = root.children[0].name;
+            String id = root.children[1].name;
+            Boolean isNew = inCurrentScope(id);
+
+            if (isNew)
+            {
+                ScopeNode currentScope = this.scopes[this.currentScope];
+                currentScope.scopeMembers.Add(new ScopeElement(type, id));
+                print("id: " + id + " created in scope " + this.currentScope + ".");
+            }
+            else
+            {
+                print("~~~Error: id " + id + " already exists in scope " + this.currentScope + ".");
+                this.errorCount++;
+            }
+        }
+
+        private void generateAssignment(Node root)
+        {
+            Node assignOpNode = root.children[0];
+            Node idNode = assignOpNode.children[0];
+            Node valueNode = assignOpNode.children[1];
+            List<int> scopeLocations = findScope(idNode.name);
+            int scopeIndex = scopeLocations[0];
+            int locationInScope = scopeLocations[1];
+            if (scopeIndex != -1 && locationInScope != -1)
+            {
+                ScopeElement idToModify = this.scopes[scopeIndex].scopeMembers[locationInScope];
+                List<Object> exprValues = generateExpr(valueNode);
+                String exprType = exprValues[0].ToString();
+                if (exprType == idToModify.type)
+                {
+                    idToModify.value = exprValues[1];
+                    idToModify.isInitialized = true;
+                    print("id: " + idNode.name + " assigned " + exprValues[1].ToString() + ".");
+                }
+                else
+                {
+                    print("~~~Error: id " + idNode.name + " is does not match the type of the expression being assigned.");
+                    this.errorCount++;
+                }
+            }
+            else
+            {
+                print("~~~Error: id " + idNode.name + " is not declared in its scope or any parent scope.");
+                this.errorCount++;
+            }
+        }
+
+        private void generatePrint(Node root)
+        {
+            generateExpr(root.children[0]);
+        }
+
+        private List<Object> generateExpr(Node root)
+        {
+            return new List<Object>() {"int", 2 };
+        }
+
+        private Boolean inCurrentScope(String id)
+        {
+            Boolean isNewId = true;
+            ScopeNode currentScopeNode = this.scopes[this.currentScope];
+            for (int i = 0; i < currentScopeNode.scopeMembers.Count; i++)
+            {
+                if (id == currentScopeNode.scopeMembers[i].id.ToString())
+                {
+                    isNewId = false;
+                }
+            }
+            return isNewId;
+        }
+
+        private List<int> findScope(String id)
+        {
+            Boolean foundId = false;
+            int scopeIndex = -1;
+            int locationInScope = -1;
+            ScopeNode currentScopeNode = this.scopes[this.currentScope];
+            int scopePlaceHolder = this.currentScope;
+            while (this.currentScope != -1 && !foundId)
+            {
+                foundId = !inCurrentScope(id);
+                if (!foundId)
+                {
+                    this.currentScope = currentScopeNode.parentId;
+                    if (this.currentScope != -1)
+                    {
+                        currentScopeNode = this.scopes[this.currentScope];
+                    }
+                }
+            }
+
+            if (foundId)
+            {
+                scopeIndex = this.currentScope;
+                this.currentScope = scopePlaceHolder;
+                currentScopeNode = this.scopes[scopeIndex];
+                for (int i = 0; i < currentScopeNode.scopeMembers.Count; i++)
+                {
+                    if (id == currentScopeNode.scopeMembers[i].id)
+                    {
+                        locationInScope = i;
+                    }
+                }
+            }
+            return new List<int>() { scopeIndex, locationInScope };
         }
 
         private void print(Object message)
@@ -99,12 +234,12 @@ namespace FlatPiler
     class ScopeElement
     {
         public string type;
-        public char id;
+        public string id;
         public Boolean isInitialized = false;
         public Boolean isUsed = false;
         public Object value;
 
-        public ScopeElement(string type, char id)
+        public ScopeElement(string type, string id)
         {
             this.type = type;
             this.id = id;
