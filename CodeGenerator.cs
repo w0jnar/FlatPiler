8c0e3079eb58d84a681aba4f16e788a35faead1e
Thomas Wojnar <wojnarthomas@gmail.com>
2015-04-29 21:49:57 -0400

    IntExpr Code Gen "done."
    Most of this relies on other parts being working correctly, so for now,
    testing is not not possible. The next step is print, so we can confirm
    print (2+2) works.

--- CodeGenerator.cs
+++ CodeGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,11 +10,25 @@ namespace FlatPiler
 {
     class CodeGenerator : CompilerElement
     {
+        private static Regex isDigit = new Regex("^[0-9]$");
+        private static Regex isChar = new Regex("^[a-z]$");
+        private static Regex isPlus = new Regex("^[\\+]$");
+        private static Regex isString = new Regex("\"[a-z ]*\"");
+        private static Regex isBadString = new Regex("\"[a-zA-Z ]*\"");
+        private static Regex isWhiteSpace = new Regex("[\\s]");
+
         private Node astRoot;
         private int errorCount = 0;
         private int index = 0;
         private int currentScope = 0;
         private int currentScopeId = 0;
+
+        private List<string> generatedCode = new List<string>();
+        private int tempCount = 0;
+        private int offset = 0;
+        private string currentTemp;
+
+        private List<Tuple<string, string, int, int>> staticData = new List<Tuple<string, string, int, int>>();
 
         public CodeGenerator(Node astRoot, TextBox taOutput)
             : base(taOutput)
@@ -23,10 +38,16 @@ namespace FlatPiler
 
         public void generateCode()
         {
+            buildPrintMessage("");
+            buildPrintMessage("~~~Now Starting Code Generation from AST");
             for (int i = 0; i < astRoot.children.Count; i++)
             {
                 generateFromNode(astRoot.children[i]);
             }
+
+            this.generatedCode[this.index++] = "00";
+
+            string currentJump = "";
         }
 
         private void generateFromNode(Node currentNode)
@@ -34,6 +55,7 @@ namespace FlatPiler
             string currentNodeName = currentNode.name;
             if (currentNodeName == "Print")
             {
+                generatePrintCode(currentNode);
             }
             else if (currentNodeName == "Assignment")
             {
@@ -50,6 +72,84 @@ namespace FlatPiler
             else if (currentNodeName == "Statement List")
             {
             }
+        }
+
+        private void generatePrintCode(Node printNode)
+        {
+            Node exprNode = printNode.children[0];
+            buildPrintMessage("--Generating Print Code.");
+            Tuple<string, string> exprTuple = generateExprTuple(exprNode);
+        }
+
+        private Tuple<string, string> generateExprTuple(Node exprNode)
+        {
+            string exprNodeName = exprNode.name;
+            Tuple<string, string> exprTuple;
+            if (isDigit.IsMatch(exprNodeName))
+            {
+                exprTuple = new Tuple<string, string>("digit", exprNodeName);
+            }
+            else if (isPlus.IsMatch(exprNodeName))
+            {
+                exprTuple = new Tuple<string, string>("digit", generateIntExpr(exprNode));
+            }
+            else
+            {
+                exprTuple = new Tuple<string, string>("digit", exprNodeName);
+            }
+            return exprTuple;
+        }
+
+        private string generateIntExpr(Node intExprNode)
+        {
+            Tuple<string, string> leftExpr = generateExprTuple(intExprNode.children[0]);
+            Tuple<string, string> rightExpr = generateExprTuple(intExprNode.children[1]);
+            string exprString;
+            if (rightExpr.Item2[0] == 'T')
+            {
+                string tempName = "S" + this.tempCount.ToString();
+                tempCount++;
+                this.generatedCode[this.index++] = "A9";
+
+                if (int.Parse(leftExpr.Item2).ToString("x").Length == 1)
+                {
+                    this.generatedCode[this.index++] = "0" + int.Parse(leftExpr.Item2).ToString("x").ToUpper();
+                }
+                else
+                {
+                    this.generatedCode[this.index++] = int.Parse(leftExpr.Item2).ToString("x").ToUpper();
+                }
+
+                this.generatedCode[this.index++] = "6D";
+                this.generatedCode[this.index++] = rightExpr.Item2;
+                this.generatedCode[this.index++] = "XX";
+
+                this.generatedCode[this.index++] = "8D";
+                this.generatedCode[this.index++] = this.currentTemp;
+                this.generatedCode[this.index++] = "XX";
+
+                exprString = this.currentTemp;
+            }
+            else
+            {
+                exprString = (int.Parse(leftExpr.Item2) + int.Parse(rightExpr.Item2)).ToString("x");
+            }
+
+            return exprString;
+        }
+
+        private void createTemp(string tempName)
+        {
+            buildPrintMessage("--Creating Temporary Variable " + tempName + ".");
+            if (this.currentTemp == null)
+            {
+                this.currentTemp = "T0";
+            }
+            else
+            {
+                this.currentTemp = "T" + (int.Parse(this.currentTemp[1].ToString()) + 1).ToString();
+            }
+            this.staticData.Add(new Tuple<string, string, int, int>(this.currentTemp, tempName, this.currentScopeId, this.offset++));
         }
     }
 }
